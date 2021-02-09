3ea3a7b9c5c8005f1bcc075adb24ebaeb6d98efc
Mathias <math8802>
2021-02-09 22:06:21 +0100

    finished program

---
+++ src/main/java/com/example/friyay/controllers/IsItFriday.java
@@ -0,0 +1,18 @@
+package com.example.friyay.controllers;
+
+import com.example.friyay.services.DayOfWeek;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IsItFriday {
+    @ResponseBody
+    @GetMapping("isItFriday")
+    public String dayOfTheWeek(){
+
+        DayOfWeek dayOfTheWeek = new DayOfWeek();
+
+        return dayOfTheWeek.getDayOfWeek();
+    }
+}
---
+++ src/main/java/com/example/friyay/services/DayOfWeek.java
@@ -0,0 +1,34 @@
+package com.example.friyay.services;
+
+import java.time.Clock;
+import java.time.LocalDate;
+
+public class DayOfWeek {
+
+    public String getDayOfWeek(){
+        java.time.DayOfWeek dayOfWeek = getDayOfTheWeek();
+
+        //For testing
+        java.time.DayOfWeek dayOfWeekFridayTest = java.time.DayOfWeek.FRIDAY;
+
+        String str = "<center><h1>Is it Friday?</h1></center><br>";
+
+        if (dayOfWeek == java.time.DayOfWeek.FRIDAY) {
+            return str + friday();
+        }
+        return str + "<center><h1>No, it is " + dayOfWeek + "</h1></center>";
+
+    }
+
+    private java.time.DayOfWeek getDayOfTheWeek() {
+        Clock clock = Clock.systemUTC();
+        LocalDate localDate = LocalDate.now(clock);
+        return java.time.DayOfWeek.from(localDate);
+    }
+
+    private String friday(){
+        return "<center><h1 style=\"background-color:FF99CC; \"> It's Friday!!!!!</h1></center> " +
+                "<center><iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/j9V78UbdzWI?start=25\" frameborder=\"0\" allow=\"accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></center>";
+
+    }
+}
