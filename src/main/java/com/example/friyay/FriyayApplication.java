67fff3d381a11e2a3bf9057fc37c3e4efda358f0
Mathias <math8802>
2021-02-09 14:41:24 +0100

    first

---
+++ src/main/java/com/example/friyay/FriyayApplication.java
@@ -0,0 +1,13 @@
+package com.example.friyay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriyayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriyayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/friyay/FriyayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friyay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriyayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
